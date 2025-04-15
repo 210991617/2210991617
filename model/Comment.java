afea28e2fb158acc9e640c263b939e85df733d6d
Harsh2405 <122768171+210991617@users.noreply.github.com>
2025-04-15 12:32:15 +0530

    Update Comment.java

--- model/Comment.java
+++ model/Comment.java
@@ -3,5 +3,5 @@ package com.example.socialmedia.model;
 public class Comment {
     private int postId;
 
-    // Getters and setters
+    // Here again use of Getters and setters so that we can take input and then show the posts as output
 }
