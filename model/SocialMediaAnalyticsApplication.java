100925e98fcbc59d9868752bafc288e4ccaaf615
Harsh2405 <122768171+210991617@users.noreply.github.com>
2025-04-15 12:31:17 +0530

    Update SocialMediaAnalyticsApplication.java

--- model/SocialMediaAnalyticsApplication.java
+++ model/SocialMediaAnalyticsApplication.java
@@ -1,8 +1,8 @@
 package com.example.socialmedia;
-
+// Herer the packages have been imported for the work to be continue in springboot
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// To create a springboot application
 @SpringBootApplication
 public class SocialMediaAnalyticsApplication {
     public static void main(String[] args) {
