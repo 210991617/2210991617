0291ef3b34340e849deaba34d2c3d5f3b60e27c3
Harsh2405 <122768171+210991617@users.noreply.github.com>
2025-04-15 12:21:21 +0530

    Create SocialMediaAnalyticsApplication.java

---
+++ SocialMediaAnalyticsApplication.java
@@ -0,0 +1,11 @@
+package com.example.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaAnalyticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaAnalyticsApplication.class, args);
+    }
+}
