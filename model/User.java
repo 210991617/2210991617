a0d646e86957ad6dfb5fc4db7d24825077607641
Harsh2405 <122768171+210991617@users.noreply.github.com>
2025-04-15 12:37:45 +0530

    Update User.java

--- model/User.java
+++ model/User.java
@@ -1,8 +1,16 @@
-package com.example.socialmedia.model;
+package com.example.social.model;
 
 public class User {
     private int id;
     private String name;
 
-    //  Use of Getters and setters to take input and show the output
+    public User() {}
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Here we use Getters and setters to take inpurt and give outputs
 }
+
